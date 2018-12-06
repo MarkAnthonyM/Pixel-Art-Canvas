5ab689806e94bb472c2441e5dc86151161b2bc9c
Mark Anthony <markanthonymartinez9@gmail.com>
2018-12-06 02:31:36 -0600

    initial commit

---
+++ JS/design.js
@@ -0,0 +1,586 @@
+//Array that contains image rgb data
+var nesImage = {
+  "width": "",
+  "height": "",
+  "tests": [
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(223, 223, 223)',
+      'rgb(223, 223, 223)',
+      'rgb(195, 195, 195)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(217, 17, 27)',
+      'rgb(182, 14, 22)',
+      'rgb(121, 9, 17)',
+      'rgb(121, 9, 17)',
+      'rgb(121, 9, 17)',
+      'rgb(182, 14, 22)',
+      'rgb(217, 17, 27)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(182, 14, 22)',
+      'rgb(217, 17, 27)',
+      'rgb(182, 14, 22)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(182, 14, 22)',
+      'rgb(217, 17, 27)',
+      'rgb(182, 14, 22)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(217, 17, 27)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(87, 87, 240)',
+      'rgb(72, 65, 222)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(152, 12, 19)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(182, 14, 22)',
+      'rgb(152, 12, 19)',
+      'rgb(121, 9, 17)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(0, 0, 0)',
+      'rgb(87, 87, 240)',
+      'rgb(87, 87, 240)',
+      'rgb(72, 65, 222)',
+      'rgb(72, 65, 222)',
+      'rgb(60, 62, 164)',
+      'rgb(60, 62, 164)',
+      'rgb(0, 0, 0)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(152, 12, 19)',
+      'rgb(121, 9, 17)',
+      'rgb(152, 12, 19)',
+      'rgb(121, 9, 17)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 158, 140)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 158, 140)',
+      'rgb(185, 98, 85)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(185, 98, 85)',
+      'rgb(255, 158, 140)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(185, 98, 85)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(253, 159, 162)',
+      'rgb(253, 159, 162)',
+      'rgb(185, 98, 85)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(192, 122, 112)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(192, 122, 112)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(185, 98, 85)',
+      'rgb(253, 159, 162)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(255, 213, 191)',
+      'rgb(253, 159, 162)',
+      'rgb(185, 98, 85)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(27, 27, 167)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(163, 80, 68)',
+      'rgb(163, 80, 68)',
+      'rgb(163, 80, 68)',
+      'rgb(0, 0, 0)',
+      'rgb(0, 0, 0)',
+      'rgb(27, 27, 167)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ],
+    ['rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(0, 0, 0)',
+      'rgb(27, 27, 167)',
+      'rgb(27, 27, 167)',
+      'rgb(122, 97, 48)',
+      'rgb(245, 185, 74)',
+      'rgb(245, 185, 74)',
+      'rgb(245, 185, 74)',
+      'rgb(245, 185, 74)',
+      'rgb(245, 185, 74)',
+      'rgb(122, 97, 48)',
+      'rgb(27, 27, 167)',
+      'rgb(27, 27, 167)',
+      'rgb(0, 0, 0)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)',
+      'rgb(50, 126, 129)'
+    ]
+  ]
+};
+
+//Variable to be used
+const bodyDiv = $('body');
+const colorCircles = $('.color-rows');
+const gameBoy = $('.pixel-menu');
+const table = $('table');
+const tableRow = $('tr');
+const tableData = $('td');
+const colorWheel = $('#colorPicker');
+const inputHeight = $('#inputHeight');
+const inputWidth = $('#inputWidth');
+let clickedColor = "rgb(255, 255, 255)";
+let rowNum = "";
+let colNum = "";
+let buttonDown = false;
+
+$(document).ready(function() {
+  console.log("The document is now ready!")
+
+//These functions gather values of size input boxes
+  inputHeight.keyup(function() {
+    rowNum = $(this).val();
+  })
+
+  inputWidth.keyup(function() {
+    colNum = $(this).val();
+  })
+
+//This function creates the grid based on size input values
+  function makeGrid(data1, data2) {
+    table.empty();
+
+    for (var i = 0; i < data1; i++) {
+      table.append('<tr></tr>');
+    }
+
+    for (var y = 0; y < data2; y++) {
+      $('tr').append('<td></td>');
+    }
+
+    $('td').each(function() {
+      $(this).css('background-color', 'white');
+      $(this).toggleClass('grid-square');
+    });
+  }
+
+//function that resizes gameboy based on input values
+function sizeGameboy(widthValue) {
+  if (widthValue <= 10) {
+    gameBoy.css("width", "25%");
+    colorCircles.css("height", "20px");
+  } else if (widthValue <= 15) {
+    gameBoy.css("width", "30%");
+    colorCircles.css("height", "25px");
+  } else if (widthValue <= 20) {
+    gameBoy.css("width", "40%");
+    colorCircles.css("height", "35px");
+  } else if (widthValue <= 25) {
+    gameBoy.css("width", "45%");
+    colorCircles.css("height", "40px");
+  } else {
+    gameBoy.css("width", "55%");
+    colorCircles.css("height", "45px");
+  }
+}
+
+//Function to bring up color wheel from d-pad
+  $('.left').on('click', function() {
+    $('#colorPicker').click();
+  })
+
+//Code to change to pointer
+  $('.up').on('click', function() {
+    $('body').css('cursor', 'default');
+    colorWheel.val('#000000');
+  });
+
+//code to reset grid
+  $('.right').on('click', function() {
+    $('td').each(function() {
+      $(this).css('background-color', 'white');
+    });
+  });
+
+//Code to change to eraser
+  $('.down').on('click', function() {
+    bodyDiv.css('cursor', 'url("https://png.icons8.com/office/30/333333/erase.png"), pointer');
+    $('#colorPicker').val('#ffffff');
+  });
+
+//Code to save images
+  $(".middle").click(function() {
+    html2canvas(table, {
+      onrendered: function(table) {
+        let imgData = table.toDataURL("image/png");
+        let doc = new jsPDF("p", "mm");
+        doc.addImage(imgData, "PNG", 50, 50);
+        doc.save("pixelart.pdf");
+      }
+    });
+  });
+
+//Function to convert rgb data to hex
+  function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+//Variable that sets individual square color
+  let pixelColor = function(element) {
+    let colorSelector = $('#colorPicker').val();
+    $(element).css('background-color', colorSelector);
+  }
+
+//code to pick colors from buttons
+  $('.circle').on('click', function() {
+    clickedColor = $(this).css('background-color');
+    var rgbValues = clickedColor.split("(")[1].split(")")[0].split(", ");
+    var convertedArray = rgbValues.map(Number);
+    var convertedHex = rgbToHex(convertedArray[0], convertedArray[1], convertedArray[2]);
+    colorWheel.val(convertedHex);
+});
+
+//Fills in pixels if mouse button clicked or held down
+  table.on('mousedown', '.grid-square', function(event) {
+    pixelColor(event.target);
+    buttonDown = true;
+  });
+
+//sets up a boolean to use in a conditional statement
+  table.on('mouseup', '.grid-square', function(event) {
+    buttonDown = false;
+  });
+
+//Function to paint adjacent squares if mouse button is held down
+  table.on('mouseenter', '.grid-square', function(event) {
+    if (buttonDown === true) {
+      pixelColor(event.target);
+    }
+  });
+
+//function to load the example image. Credit to Julia Christiakova
+  var importImage = function(data) {
+    inputWidth.val(data.width);
+    inputHeight.val(data.height);
+    makeGrid(20, 20);
+    jQuery.each(table.find('tr'), function(i, node) {
+      jQuery.each($(node).find('td'), function(j, node) {
+        $(node).css('background-color', data.tests[i][j]);
+      });
+    });
+  }
+
+  importImage(nesImage);
+
+  $('.engage').on('click', function() {
+    makeGrid(rowNum, colNum);
+    sizeGameboy(colNum);
+  });
+});
